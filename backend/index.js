365144f0ecd49df962d5968ccf0eb518aa49cc48
manishm96 <89826948+manishm96@users.noreply.github.com>
2022-05-12 18:13:28 -0700

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -14,9 +14,7 @@ if (!config.get("jwtPrivateKey")) {
 app.use(cors());
 app.use(express.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
-// simple route
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to Hotel Booking Application." });
 });
@@ -27,6 +25,53 @@ require("./routes/roomRoutes")(app);
 require("./routes/reservationRoutes")(app);
 
 
+app.post("/signup", async function (req, res) {
+    const salt = await bcrypt.genSalt(10);
+    req.body.password = await bcrypt.hash(req.body.password, salt);
+    console.log("Registering New User");
+    const newUser = {
+      firstName: req.body.name,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+      address: req.body.address,
+    };
+    new user(newUser).save((error, data) => {
+      if (error) {
+        res.status(500).end("Error Occured");
+      } else {
+        data.password = "";
+        var JSONStr = JSON.stringify(data);
+        res.status(200).end(JSONStr);
+      }
+    });
+  });
+
+  app.post("/login", async function (req, res) {
+    user.findOne({ email: req.body.email }, (error, user) => {
+      if (error) {
+        res.status(500).end("User Not Found");
+      }
+      if (user) {
+        bcrypt.compare(
+          req.body.password,
+          user.password,
+          function (err, matchPassword) {
+            if (err) return error;
+            if (matchPassword) {
+              user.password = "";
+              var JSONStr = JSON.stringify(user);
+              res.status(200).end(JSONStr);
+            } else {
+              res.status(500).end("UnSuccessful Login");
+            }
+          }
+        );
+      } else {
+        res.status(500).end("UnSuccessful Login");
+      }
+    });
+  });
 
 const port = config.get("port");
 app.listen(port, () => console.log(`Listening to port ${port}...`));
